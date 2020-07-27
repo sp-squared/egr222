cb5c3bb62745a028d160b5690d080769ed12d71a
Colin A. Morris <44488139+sp-squared@users.noreply.github.com>
2020-07-26 20:58:15 -0700

    Java Basics

---
+++ textbook_notes/chapter1/Chapter_1/src/java_basics/PrintDIff.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class PrintDIff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("To be ");
+		System.out.print("or not to be.");
+		System.out.print("That is ");
+		System.out.println("the question.");
+		System.out.print("This is");
+		System.out.println(" for the whole family!");
+	}
+
+}
